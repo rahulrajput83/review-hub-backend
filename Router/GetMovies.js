54664f84c501d143dae53b5a26168d1fc988ef7e
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-30 18:54:41 +0530

    Added Route for single movie detail

--- Router/GetMovies.js
+++ Router/GetMovies.js
@@ -17,7 +17,26 @@ router.get('/all-movies', (req, res) => {
     .catch((err) => {
         res.status(503).json({message: 'Something went wrong...', err: err})
     })
-})
+});
+
+
+/* Get Single Movie Detail Route */
+router.get('/movie/:id', (req, res) => {
+    MoviesModel.findOne({_id: req.params.id})
+    .then((value) => {
+        if(value) {
+            res.status(200).json({message: 'Success', data: value})
+        }
+        else {
+            res.status(200).json({message: 'No Movie Found.'})
+        }
+    })
+    .catch((err) => {
+        res.status(503).json({message: 'Something went wrong...', err: err})
+    })
+});
+
+
 
 /* Exports Router */
 module.exports = router;
