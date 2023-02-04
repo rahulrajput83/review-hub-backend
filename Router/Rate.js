3f079c28e1659e5703130df6296b0d06cbdd8db7
Rahul Rajput <rahulrajput21112@gmail.com>
2023-02-04 12:54:31 +0530

    Updated Rate Route

--- Router/Rate.js
+++ Router/Rate.js
@@ -5,7 +5,7 @@ const verify = require('../Middleware/AuthJWT');
 
 /* Rate Movie Route */
 router.put('/rate', verify, (req, res) => {
-    MoviesModel.updateOne({_id: req.body.movieId}, {$set: {"rating": req.body.rating}, $push: {"rated": {user: {_id: req.user._id, userName: req.body.userName}, rating: req.body.rated}}})
+    MoviesModel.updateOne({_id: req.body.movieId}, {$set: {"rating": req.body.rating}, $push: {"rated": {user: {_id: req.user._id, userName: req.user.userName}, rating: req.body.rated}}})
     .then(() => {
         res.status(200).json({message: 'Success'})
     })
